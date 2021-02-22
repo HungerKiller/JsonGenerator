8b173fae230e8417f37b2e14a54066ed0a792d0b
HungerKiller <zhongshui.wang.chn@gmail.com>
2021-02-22 14:42:13 +0100

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
